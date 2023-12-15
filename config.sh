5884e7d2c4e1df911867c1a631e9785577c38c26
Isak-Landin <utbisalan@gmail.com>
2023-12-15 14:58:16 +0100

    -

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Currently the only accepted unit of time is h
+Timespan="24h"
+
+database_name="loginattemptsdb"
+
+table_login_attempts="login_attempts"
+table_dropped_users="dropped_users"
+table_good_users="good_users"
